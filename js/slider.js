9a99d7779bfa989ce9ec15b6facc5f7237f272e9
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-10-20 14:04:45 +0300

    addded info

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$('.fade').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  dots: true,
+  infinite: true,
+  speed: 700,
+  fade: true,
+  cssEase: 'linear'
+});
